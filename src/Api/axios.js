7d15655ab4ab1e1091eb4d6ef53d30219a3f5ad8
501357844 <501357844@qq.com>
2019-07-16 10:10:10 +0800

    完成7月15日的部分任务

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,12 +1,19 @@
 // 封装axios
 import axios from 'axios'
+import JSONBig from 'json-bigint'
 
 // 创建一个axios对象 使用｛｝的配置项
 const instance = axios.create({
-  baseURL: 'http://ttapi.research.itcast.cn/mp/v1_0/'
+  baseURL: 'http://ttapi.research.itcast.cn/mp/v1_0/',
   // headers: {
   //   Authorization: 'Bearer ' + JSON.parse(window.sessionStorage.getItem('hm73-toutiao')).token
   // }
+  transformResponse: [(data) => {
+    if (data) {
+      return JSONBig.parse(data)
+    }
+    return data
+  }]
 })
 
 // 请求拦截器
@@ -34,7 +41,7 @@ instance.interceptors.response.use(response => {
   // 做自己的事情 错误的时候
   // 如果响应状态码是 401 拦截到登录页面
   // error.response.status 状态码
-  if (error.response.status === 401) {
+  if (error.response && error.response.status === 401) {
     // hash 是location提供获取操作 地址栏的#后的地址的属性
     location.hash = '#/login'
   }
--- src/views/container/index.vue
+++ src/views/container/index.vue
@@ -16,17 +16,12 @@
           </el-radio-group>
         </el-form-item>
         <el-form-item label="频道：">
-          <el-select v-model="reqParams.channel_id">
-            <el-option
-              v-for="item in channelOptions"
-              :key="item.id"
-              :label="item.name"
-              :value="item.id"
-            ></el-option>
-          </el-select>
+           <!-- <my-channel v-model="reqParams.channel_id"></my-channel> -->
         </el-form-item>
         <el-form-item label="时间：">
           <el-date-picker
+            value-format="yyyy-MM-dd"
+            @change="changeDate"
             v-model="dateValues"
             type="daterange"
             range-separator="至"
@@ -35,11 +30,57 @@
           ></el-date-picker>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary">筛选</el-button>
+          <el-button type="primary" @click="search()">筛选</el-button>
         </el-form-item>
       </el-form>
     </el-card>
-    <el-card></el-card>
+    <!-- 结果容器 -->
+    <el-card>
+      <div slot="header">
+        根据筛选条件查询到
+        <b>{{total}}</b> 条结果
+      </div>
+      <!-- data中绑定的是（文章）列表数据 -->
+      <!-- 是一个数组 -->
+      <el-table :data="articles">
+        <el-table-column label="封面">
+          <template slot-scope="scope">
+            <el-image lazy :src="scope.row.cover.images[0]" style="width:100px;height:75px;">
+              <div slot="error" class="image-slot">
+                <img src="../../assets/images/error.gif" width="100px" height="75px" alt />
+              </div>
+            </el-image>
+          </template>
+        </el-table-column>
+        <el-table-column label="标题" prop="title"></el-table-column>
+        <el-table-column label="状态" prop="status">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.status === 0" type="info">草稿</el-tag>
+            <el-tag v-if="scope.row.status === 1">待审核</el-tag>
+            <el-tag v-if="scope.row.status === 2" type="success">审核通过</el-tag>
+            <el-tag v-if="scope.row.status === 3" type="waring">审核失败</el-tag>
+            <el-tag v-if="scope.row.status === 4" type="danger">已删除</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="发布时间" prop="pubdate"></el-table-column>
+        <el-table-column label="操作" width="120px">
+          <template slot-scope="scope">
+            <el-button icon="el-icon-edit" circle plain type="primary" @click="edit(scope.row.id)"></el-button>
+            <el-button icon="el-icon-delete" circle plain type="danger" @click="del(scope.row.id)"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="box">
+        <el-pagination
+          background
+          layout="prev, pager, next"
+          @current-change="changePager"
+          :current-page ="reqParams.page"
+          :page-size="reqParams.per_page"
+          :total="total"
+        ></el-pagination>
+      </div>
+    </el-card>
   </div>
 </template>
 
@@ -53,28 +94,67 @@ export default {
       // 提交给后台的筛选条件  传参
       // 数据默认是''还是null的区别,如果是null将不会发送字段
       reqParams: {
+        page: 1,
+        per_page: 20,
         status: null,
         channel_id: null,
         begin_pubdate: null,
         end_pubdate: null
       },
-      // 默认频道数据
-      channelOptions: [],
+
       // 日期控件的数据
-      dateValues: []
+      dateValues: [],
+      // 文章列表数据
+      articles: [],
+      total: 0
     }
   },
   // 获取频道数据
   created () {
-    this.getchannelOptions()
+    this.getArticles()
   },
   methods: {
-    async getchannelOptions () {
-      // 依据接口文档
-      // 此时没必要去处理错误
-      // 频道数据如果没有取成功则不会显示数据
-      const { data: { data } } = await this.axios.get('channels')
-      this.channelOptions = data.channels
+    edit (id) {
+      // this.$router.push('/push')
+      this.$router.push({ path: '/push', query: { id } })
+    },
+    del (id) {
+      // 确认框
+      this.$confirm('亲，此操作将永久删除该文章, 是否继续?', '温馨提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(async () => {
+          // 点击确认  发删除请求
+          // 后台没有任何响应  一直等待响应 导致后面代码无法执行。
+          await this.axios.delete(`articles/${id}`)
+          // 删除成功后做什么？
+          this.getArticles()
+          this.$message.success('删除成功')
+        })
+        .catch(() => {
+          // 点击取消
+        })
+    },
+    changePager (newPage) {
+      this.reqParams.page = newPage
+      this.getArticles()
+    },
+    search () {
+      this.reqParams.page = 1
+      this.getArticles()
+    },
+    changeDate (values) {
+      this.reqParams.begin_pubdate = values[0]
+      this.reqParams.end_pubdate = values[1]
+    },
+    async getArticles () {
+      const {
+        data: { data }
+      } = await this.axios.get('articles', { params: this.reqParams })
+      this.articles = data.results
+      this.total = data.total_count
     }
   }
 }
@@ -83,5 +163,9 @@ export default {
 <style lang="less" scoped>
 .el-card {
   margin-bottom: 20px;
+}
+.box {
+  text-align: center;
+  margin-top: 20px;
 }
 </style>
